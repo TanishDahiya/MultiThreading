ed78c0ee02404423a5ec2e0e3837e90f222716a0
Tanish Dahiya <48643197+TanishDahiya@users.noreply.github.com>
2026-05-11 14:25:20 +0530

    Add InvokeAllExample demonstrating task execution
    Implement example of using invokeAll with Callable tasks in Executor framework.

---
+++ Executor Framework/example6.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class InvokeAllExample {
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        List<Callable<Integer>> tasks = Arrays.asList( () -> 10, () -> 20, () -> 30);
+
+        // runs all tasks
+        List<Future<Integer>> futures = executor.invokeAll(tasks);   // invokeAll() - main thread wait until all tasks comepleted
+
+        // get results
+        for (Future<Integer> future : futures) {        // Future<Integer> future :  because future is object of integer that's why we have written
+            System.out.println(future.get());           // print all values 10,20,30 
+        }
+
+        executor.shutdown();
+    }
+}
