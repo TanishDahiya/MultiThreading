46bc67152ff089b6f23b0d85863c0b081f077056
Tanish Dahiya <48643197+TanishDahiya@users.noreply.github.com>
2026-05-11 13:11:06 +0530

    Enhance example3.java with comments on Future usage
    Added comments to clarify usage of Future with and without type parameters.

--- Executor Framework/example3.java
+++ Executor Framework/example3.java
@@ -6,14 +6,17 @@ public class FutureExample {
 
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
-        Future<Integer> future = executor.submit(() -> {
+        Future<Integer> future = executor.submit(() -> {              // we can also use Future<?> -> like this 
             return 10 + 20;
         });
 
-        Integer result = future.get();   // get returned value
+        Integer result = future.get();   // get returned value        // but if we use ? at this line Object is returned Object result = future.get()
 
         System.out.println(result);
 
         executor.shutdown();
     }
 }
+
+// We can use Future<?> when we do not care about the return type.
+// if we know exact type (Future<Integer>) then we can use with data type,  because it gives type safety and avoids casting.
